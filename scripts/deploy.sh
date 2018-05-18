e4467a91edee0124037ad1e67032de0ac28a5cb7
Vance Lucas <vance@vancelucas.com>
2018-05-18 10:39:43 -0500

    Remove old generated files (oops)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+GIT_REPO_URL=$(git config --get remote.origin.url)
+
 # Build files (will generate all files in "./dist" folder)
 NODE_ENV=production npm run build
 
-GIT_REPO_URL=$(git config --get remote.origin.url)
-
+# Copy some files we want...
 cp CNAME dist/
+cp robots.txt dist/
+
+# Deploy 'dist' to 'gh-pages' branch
 cd dist/
 git init .
 git remote add github $GIT_REPO_URL
